d02fcb398377460126ebdf3689fcaf87b1f92f4e
Keyshawn Jones <keyshawnjones29@gmail.com>
2023-03-01 12:51:01 -0600

    backend java

---
+++ src/main/java/com/kiwi/springwebsite/SpringwebsiteApplication.java
@@ -0,0 +1,14 @@
+package com.kiwi.springwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiwi/springwebsite/SpringwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiwi.springwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
